312b1d5e7d78ac9fbc914d7db5c5a4f74b925544
mandeep <singh.mr.1996@gmail.com>
2022-01-26 16:06:08 +0530

    [Mandeep] Feat: add swagger for register api

--- employee-payroll-app/build/index.js
+++ employee-payroll-app/build/index.js
@@ -17,6 +17,10 @@ var _cors = _interopRequireDefault(require("cors"));
 
 var _helmet = _interopRequireDefault(require("helmet"));
 
+var _swaggerUiExpress = _interopRequireDefault(require("swagger-ui-express"));
+
+var _swagger = _interopRequireDefault(require("../src/swagger/swagger.json"));
+
 var _routes = _interopRequireDefault(require("./routes"));
 
 var _database = _interopRequireDefault(require("./config/database"));
@@ -46,6 +50,7 @@ app.use(_express["default"].json());
 app.use((0, _morgan["default"])('combined', {
   stream: _logger.logStream
 }));
+app.use('/api-docs', _swaggerUiExpress["default"].serve, _swaggerUiExpress["default"].setup(_swagger["default"]));
 (0, _database["default"])();
 app.use("/api/".concat(api_version), (0, _routes["default"])());
 app.use(_error.appErrorHandler);
--- employee-payroll-app/src/index.js
+++ employee-payroll-app/src/index.js
@@ -4,14 +4,12 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../src/swagger/swagger.json';
 
 import routes from './routes';
 import database from './config/database';
-import {
-  appErrorHandler,
-  genericErrorHandler,
-  notFound
-} from './middlewares/error.middleware';
+import { appErrorHandler, genericErrorHandler, notFound } from './middlewares/error.middleware';
 import logger, { logStream } from './config/logger';
 
 import morgan from 'morgan';
@@ -26,6 +24,7 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('combined', { stream: logStream }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 database();
 
